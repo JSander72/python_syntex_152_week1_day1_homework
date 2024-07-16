bc79f501b9637111299198a6c2e8214454a1aa18
JSander72 <jamesearlsanders2@outlook.com>
2024-07-16 16:20:28 -0400

    final commit for submittal

--- mood_today.py
+++ mood_today.py
@@ -1,12 +1,53 @@
-mood = input("How are your feeling today?")
-print(mood)
+### Showcase Your Dance Moves!
+# Task 1: Code Correction
+weather = "sunny"
 
-happy = "That's great to hear!"
-sad = "I'm sorry to hear that."
-
-if "happy":
-    print(happy)
+if weather == "sunny":
+    print("Wear sunglasses!")
 else:
-    print(sad)
+    print("Take an umbrella!")
+
+print(""*50)
+# Task 2: Your Mood Today
+
+ask = "How are you feeling today?"
+print(ask)
+
+user_input = input()
+print(user_input)
+
+happy1 = "That's great to hear!"
+sad1 = "I'm sorry to hear that."
+
+if input == "happy":
+    print(happy1)
+elif "sad":
+    print(sad1)
+
+# I cannot get happy1 to to print with the right user_input *** I need some help with this. *** 
     
- 
+print(""*50)
+### Python Data Types and type() Function
+# Task 1: Code Correction
+
+variable_a = "Hello, World!"
+variable_b = 23
+variable_c = 3.14
+variable_d = True
+
+print(type(variable_a))
+print(type(variable_b))
+print(type(variable_c))
+print(type(variable_d))
+
+### Arithmetic Operations in Daily Life
+# Task: Grocery STore Math
+
+bread = 2.75
+peanut_butter= 4.50
+jelly = 3.23
+
+total_grocery_cost = bread + peanut_butter + jelly
+print(total_grocery_cost)
+
+
